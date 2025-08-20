b1dddd03e7946a77f445a370ae8067990cb15214
Ritwik Gupta <ritwikg.0501@gmail.com>
2025-08-20 12:11:56 +0530

    Re-structured the project

--- backend/global_tech/settings.py
+++ backend/global_tech/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u_vx6q8d&#mtq&u8l1%^y^vrwbqeo@ove9r#ggqbuxu80pqitj'
+SECRET_KEY = 'django-insecure-%d4@f=duih(%v156&i*@od4w4w^lqo9sa12h_8q6$^g056!@mr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Personal Apps
 ]
 
 MIDDLEWARE = [
