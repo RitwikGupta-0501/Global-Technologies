a74803a94a522b3f9913cefcafb5c2997d1df031
Ritwik Gupta <ritwikg.0501@gmail.com>
2025-12-23 21:28:58 +0530

    Completed Media Implementation for Products

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,18 +1,31 @@
 from django.db import models
 
-from django.db import models
 
 class Product(models.Model):
     class PriceTypes(models.TextChoices):
-        FIXED = 'F', 'Fixed'
-        NEGOTIABLE = 'N', 'Negotiable'
+        FIXED = "F", "Fixed"
+        NEGOTIABLE = "N", "Negotiable"
 
-    name = models.CharField(max_length=255, verbose_name="Product Name") 
+    name = models.CharField(max_length=255, verbose_name="Product Name")
     description = models.TextField(verbose_name="Product Description")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Product Price", null=True, blank=True)    
-    price_type = models.CharField(max_length=1, choices=PriceTypes.choices, default=PriceTypes.FIXED, verbose_name="Price Type")
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name="Product Price",
+        null=True,
+        blank=True,
+    )
+    price_type = models.CharField(
+        max_length=1,
+        choices=PriceTypes.choices,
+        default=PriceTypes.FIXED,
+        verbose_name="Price Type",
+    )
+    image = models.ImageField(
+        upload_to="products/", verbose_name="Product Image", null=True, blank=True
+    )
 
     def __str__(self):
         return self.name
-    
+
     # TODO: Add More Fields later as per requirement
