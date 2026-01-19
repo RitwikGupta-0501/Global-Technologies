5209c610f89dd308a1b37f85d5916922495cc88f
Ritwik Gupta <ritwikg.0501@gmail.com>
2026-01-20 01:33:58 +0530

    PROD: Added frontend url to CORS

--- backend/global_tech/settings.py
+++ backend/global_tech/settings.py
@@ -156,6 +156,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://globaltechnologies-8aciib479-ritwik-guptas-projects.vercel.app",
 ]
 
 # JWT Configuration
